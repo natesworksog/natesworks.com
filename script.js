8a8824e2bc800923512aeffa04cb5324be5d09ab
Natesworks <contact@natesworks.com>
2023-09-24 09:09:25 +0000

    Replaced SwiftOS with SwiftUI

--- script.js
+++ script.js
@@ -17,10 +17,6 @@ const open = document.querySelector('.container');
 			tl.reverse();
 		});
 
-document.getElementById("download").addEventListener("click", function() {
-	window.location.href = "https://drive.google.com/open?id=1Nae-f3gua1hKm9shzewBhjCmcr78PS67&authuser=0"; 
-});
-
-document.getElementById("discord").addEventListener("click", function() {
-	window.location.href = "https://discord.gg/SJCft8eR3Q"; 
+document.getElementById("swiftuigithub").addEventListener("click", function() {
+	window.location.href = "https://github.com/natesworks/swiftui"; 
 });
